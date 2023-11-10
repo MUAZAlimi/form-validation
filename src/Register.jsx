e9a2268b97a9c5f288d6f914d885bfb6bca4e877
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-10 10:07:48 +0100

    up

--- src/Register.jsx
+++ src/Register.jsx
@@ -4,4 +4,4 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios"
 
 const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
-const PWD-REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])()
+const PWD-REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
