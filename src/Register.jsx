4eec3a92c4d89ae9d1a0bfa0e9cce5a574def586
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-10 10:33:56 +0100

    up

--- src/Register.jsx
+++ src/Register.jsx
@@ -3,8 +3,8 @@ import {faCheck, faTimes, faInfocircle} from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios"
 
-const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const USER_REGEX = /^[A-Z][a-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const REGISTER_URL = "/register"
 
 const Register = () => {
@@ -22,7 +22,7 @@ const Register = () => {
     const [success, setSuccess] = useState(false)
 
     useEffect(() => {
-        useRef.current.focus
+        useRef.current.focus()
     }, [])
     
 
