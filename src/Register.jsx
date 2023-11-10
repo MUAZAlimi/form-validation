8067053729c81d8a23b19f654a89f33b8194c3ae
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-10 10:03:27 +0100

    up

--- src/Register.jsx
+++ src/Register.jsx
@@ -2,3 +2,5 @@ import { useState, useEffect, useRef } from "react";
 import {faCheck, faTimes, faInfocircle} from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios"
+
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$
