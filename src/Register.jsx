d3d7d88af1006a97bf12603ed7b0100ab4dd0a61
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-10 10:00:54 +0100

    up

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,3 +1,4 @@
 import { useState, useEffect, useRef } from "react";
 import {faCheck, faTimes, faInfocircle} from "@fortawesome/fontawesome-svg-core"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios"
