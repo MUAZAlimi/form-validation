f8bd0fac59135e4e0c87476b60bda19bfa75868d
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-10 10:22:05 +0100

    up

--- src/Register.jsx
+++ src/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
     const [user, setUser] = useState('')
     const [validName, setValidName] = useState(false)
     const [pwdFocus, setPwdFocus] = useState(false)
-
+    
   return (
     
   )
